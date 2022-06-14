29cb7061e2965227327c82a8900594c017b3a1ee
A-Tanner <atanner5@ewu.edu>
2022-06-14 11:08:20 -0700

    Initial commit

---
+++ Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CustomTerrain))] //links this script with the editor of CustomTerrain
+[CanEditMultipleObjects]
+public class CustomTerrainEditor : Editor
+{
+
+    //Properties
+    SerializedProperty randomHeightRange;
+
+    //Fold outs
+    bool showRandom = false;
+
+    void OnEnable() //Essentially Awake. Allows processing without rerun
+    {
+        randomHeightRange = serializedObject.FindProperty("randomHeightRange");
+        
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        CustomTerrain terrain = (CustomTerrain)target;
+        //Should use serialized values between the editor and target
+        //This allows user input to stay live in the inspector
+        //Even after script changes
+
+        showRandom = EditorGUILayout.Foldout(showRandom, "Random");
+        if (showRandom)
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Set Heights Between Random Values", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(randomHeightRange);
+            if (GUILayout.Button("Random Heights"))
+            {
+                terrain.RandomTerrain();
+            }
+        }
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
