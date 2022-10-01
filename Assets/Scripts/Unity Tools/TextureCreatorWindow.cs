352e690da65bd24d297589e62079287c12eed572
A-Tanner <atanner5@ewu.edu>
2022-10-01 15:07:25 -0700

    Add working texture preview. Save still nonfunctional

--- Assets/Scripts/Unity Tools/TextureCreatorWindow.cs
+++ Assets/Scripts/Unity Tools/TextureCreatorWindow.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using ProceduralTerrain;
 using System.IO;
 
 public class TextureCreatorWindow : EditorWindow
@@ -18,6 +19,9 @@ public class TextureCreatorWindow : EditorWindow
     bool seamless = false;
     bool enhancedView = false;
 
+    int width = 513;
+    int height = 513;
+
 
     Texture2D previewTexture;
 
@@ -30,7 +34,7 @@ public class TextureCreatorWindow : EditorWindow
 
     private void OnEnable()
     {
-        previewTexture = new Texture2D(513, 513, TextureFormat.ARGB32, false);
+        previewTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
     }
 
     private void OnGUI()
@@ -40,8 +44,8 @@ public class TextureCreatorWindow : EditorWindow
         int wSize = (int)(EditorGUIUtility.currentViewWidth - 100);
 
         filename = EditorGUILayout.TextField("Texture Name", filename);
-        perlinXScale = EditorGUILayout.Slider("X Scale", perlinXScale, 0, 0.01f);
-        perlinYScale = EditorGUILayout.Slider("Y Scale", perlinXScale, 0, 0.01f);
+        perlinXScale = EditorGUILayout.Slider("X Scale", perlinXScale, 0, 0.1f);
+        perlinYScale = EditorGUILayout.Slider("Y Scale", perlinYScale, 0, 0.1f);
         perlinXOffset = EditorGUILayout.IntSlider("X Offset", perlinXOffset, 0, 4096);
         perlinYOffset = EditorGUILayout.IntSlider("Y Offset", perlinYOffset, 0, 4096);
         perlinOctaves = EditorGUILayout.IntSlider("Octaves", perlinOctaves, 1, 10);
@@ -55,7 +59,24 @@ public class TextureCreatorWindow : EditorWindow
         GUILayout.FlexibleSpace();
 
         if (GUILayout.Button("Generate", GUILayout.Width(wSize))){
+            float pixelValue;
+            Color pixelColor;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    pixelValue = Utils.FractalBrownianMotion(y * perlinXScale,
+                    x * perlinYScale,
+                    perlinOctaves,
+                    perlinPersistance,
+                    perlinXOffset,
+                    perlinYOffset) * perlinHeightScale;
 
+                    pixelColor = new Color(pixelValue, pixelValue, pixelValue, alpha ? pixelValue : 1);
+                    previewTexture.SetPixel(x, y, pixelColor);
+                }
+            }
+            previewTexture.Apply(false, false);
         }
 
         GUILayout.FlexibleSpace();
